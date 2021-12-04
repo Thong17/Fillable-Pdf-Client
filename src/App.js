c6fcd42e93386e629046108e1b585a47d43602b2
Thong <chhanbunthong@gmail.com>
2021-12-04 17:38:38 +0700

    create login route in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './views/Login'
+
+const App = () => {
+    return (
+        <Router>
+          <Routes>
+            <Route path="/login" exact element={<Login />} />
+          </Routes>
+        </Router>
+    )
 }
 
 
-export default App;
+export default App
